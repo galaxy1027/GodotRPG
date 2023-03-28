e9d785d8a150809a17f4b8ea705b35d918524205
Branford Coleman <branford2001@gmail.com>
2023-03-27 18:44:56 -0700

    added animation for player movment

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
 @export var speed: int = 35
+@onready var animations = $AnimationPlayer
 
 func handle_input():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
 	
+func player_animation():
+	if velocity.x < 0: animations.play("walk_left")
+	elif velocity.x > 0: animations.play("walk_right")
+	elif velocity.y > 0: animations.play("walk_down")
+	elif velocity.y < 0: animations.play("walk_up")
+	else: animations.stop()
+	
 func _physics_process(delta):
 	handle_input()
 	move_and_slide()
+	player_animation()
